01bf243a2b8c9d41b231b88fe606053c39a4cf8c
samtes <sweldemariam@gmail.com>
2014-06-18 16:09:17 -0500

    :sun: Did some of the styling

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -16,7 +16,7 @@ feature "User signs up" do
 
     click_link "Sign out"
     click_link "Sign in"
-    fill_in "Email", with: "joe@example.com"
+    fill_in "Email/Username", with: "joe@example.com"
     fill_in "Password", with: "mypassword"
     click_button "Sign in"
     page.should have_content "Signed in successfully."
@@ -63,29 +63,28 @@ feature "User signs up" do
     page.should have_error("doesn't match Password", on: "Password confirmation")
   end
 
-=begin
   scenario "failed signup because invalid characters in username" do
     visit '/'
-    click_link "I'm Ready!"
+    click_link "Sign up"
     fill_in "Email", with: "joe@example.com"
     fill_in "Username", with: "joe@example"
     fill_in "Password", with: "mypassword"
     fill_in "Password confirmation", with: "mypassword"
     click_button "Sign up"
-    page.should_not have_content "Welcome to Squmblr"
+    page.should_not have_content "Welcome! You have signed up successfully."
     page.should have_content "Your account could not be created."
     page.should have_error("username can only contain letters", on: "Username")
   end
 
   scenario "failed signup because invalid characters in username" do
     visit '/'
-    click_link "I'm Ready!"
+    click_link "Sign up"
     fill_in "Email", with: "joe@example.com"
     fill_in "Username", with: "joe joe"
     fill_in "Password", with: "mypassword"
     fill_in "Password confirmation", with: "mypassword"
     click_button "Sign up"
-    page.should_not have_content "Welcome to Squmblr"
+    page.should_not have_content "Welcome! You have signed up successfully."
     page.should have_content "Your account could not be created."
     page.should have_error("username can only contain letters", on: "Username")
   end
@@ -97,5 +96,4 @@ feature "User signs up" do
   scenario "user receives welcome email" do
     # PR 2: Welcome Emails
   end
-=end
 end
