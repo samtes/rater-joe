1337608c28e3301a00422cf912626a0a0b12538a
samtes <sweldemariam@gmail.com>
2014-06-26 21:39:54 -0500

    :fire: removed the example share tool

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :create]
+
 
   # GET /reviews
   # GET /reviews.json
